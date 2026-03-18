05d93692ea1faccac6a3e21bfbe211683cfaa93e
jon jones <jonjones@jons-MacBook-Pro.local>
2026-03-18 13:37:38 +0800

    Add t.me to image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { hostname: "avatars.githubusercontent.com" },
       { hostname: "github.com" },
+      { hostname: "t.me" },
     ],
   },
 };
